368ee77a4d5abeb283bf900092fe5625a584b1ab
Tim Bright <tim@successr.ai>
2026-06-15 00:08:24 -0600

    implemented outer agent loop

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+)
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// TODO: build harness (agent, registry) and call h.RunSession(ctx, os.Stdin, os.Stdout)
+
+	_ = ctx
 	fmt.Println("Hello, World!")
 }
